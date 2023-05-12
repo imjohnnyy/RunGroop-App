1c27a664de7a2bd7aaa79d0b09f6f2fa0c0b3910
imjohnnyy <ldong4056@gmail.com>
2023-05-13 00:21:58 +1200

    Added other models, and enums (to assign constant names to a group of numeric integer values)

---
+++ RunGroopWebApp/Models/AppUser.cs
@@ -0,0 +1,14 @@
+﻿using System.Diagnostics;
+
+namespace RunGroopWebApp.Models
+{
+    public class AppUser
+    {
+        public int? Pace { get; set; }
+        public int? Mileage { get; set; }
+        public Address? Address { get; set; }
+        public ICollection<Club> Clubs { get; set; }
+        public ICollection<Race> Races { get; set; }
+
+    }
+}
