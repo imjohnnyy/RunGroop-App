1a7f5b2a7d48a98ec5dec649d0b23d1828423688
imjohnnyy <ldong4056@gmail.com>
2023-05-12 23:57:51 +1200

    Setting up 'Address' Model

---
+++ RunGroopWebApp/Models/Address.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+// Think of a Model as a database table
+
+namespace RunGroopWebApp.Models
+{
+    public class Address
+    {
+        [Key]  //This [key] is what is referring to a primary key
+
+        public int Id { get; set; }  //'public' keyword allows us to access this 'Id' in other places of our app, 'private' is the opposite
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+
+    }
+}
